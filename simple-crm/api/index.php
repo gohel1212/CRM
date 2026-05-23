0c815e79c666289884866f2b81dd25286d51d623
Darshan Gohel <goheldarshan987@gmail.com>
2026-05-23 23:31:49 +0530

    Configure E_ALL but E_DEPRECATED excluded in error_reporting

--- simple-crm/api/index.php
+++ simple-crm/api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// Enable error display for debugging
-error_reporting(E_ALL);
+// Enable fatal error display but suppress deprecation warnings
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 ini_set('display_errors', '1');
 ini_set('log_errors', '1');
 

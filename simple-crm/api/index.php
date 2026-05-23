062ae9a2cbd20a158dc030fd129af340f6a6b3ad
Darshan Gohel <goheldarshan987@gmail.com>
2026-05-23 23:22:04 +0530

    Suppress deprecation warnings and disable display_errors in production

--- simple-crm/api/index.php
+++ simple-crm/api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// Suppress deprecation warnings and disable display_errors in production
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+
 // Vercel serverless workaround: Create storage folders in writeable /tmp directory
 $storageDirs = [
     '/tmp/storage/framework/views',
